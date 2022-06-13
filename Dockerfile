60ca7200b58c9f7c54e60034c52a09697b8ae17d
SPrathaban <sprathaban@kingpin.global>
2022-06-13 13:25:33 +0530

    Deploy

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:lts-alpine
+
+# Create app directory
+WORKDIR /usr/src/app
+COPY package*.json ./
+
+RUN npm install 
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
+EXPOSE 3002
+CMD [ "node", "app.js" ]
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const routes = require('./router/router');
 const db = require('./db')
-const data1 = "mongodb://localhost:27017/eveit?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const data1 = "mongodb://prathapDB:prathapDB@docdb-2022-06-13-06-48-15.cluster-cvkidzgdqwpg.us-west-2.docdb.amazonaws.com:27017/?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 db.connect(data1)
 const corsOptions = {
     origin: (origin, callback) => {
@@ -29,3 +29,25 @@ app.set('port', 3002);
             `Backend is running with environment 3002`
         );
     });
+
+    // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAOW86T71DRcdhQBZGvjBBb-r959eE37YE",
+//   authDomain: "taghive-2477d.firebaseapp.com",
+//   projectId: "taghive-2477d",
+//   storageBucket: "taghive-2477d.appspot.com",
+//   messagingSenderId: "1083774669884",
+//   appId: "1:1083774669884:web:25ea49e2e67ec359839f39",
+//   measurementId: "G-65KFK3YLGR"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
